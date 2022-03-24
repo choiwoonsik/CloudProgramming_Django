776eedc7ae6f86f2f5fef8d46295b7848f4ef880
woonsik <dnstlr2933@naver.com>
2022-03-24 15:39:42 +0900

    블로그 리스트 페이지 작성

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
-# Create your views here.
+
 def index(request):
     posts = Post.objects.all().order_by('-pk')
 
@@ -10,5 +10,17 @@ def index(request):
         'blog/index.html',
         {
             'posts': posts,
+        }
+    )
+
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
         }
     )
