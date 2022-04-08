1c24464ad4b7ece186ea719ccd55d19c39add1a6
woonsik <dnstlr2933@naver.com>
2022-04-08 15:34:10 +0900

    author 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 import os.path
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -13,12 +14,19 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+
     # methods
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
 
     def get_file_name(self):
         return os.path.basename(self.attached_file.name)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
