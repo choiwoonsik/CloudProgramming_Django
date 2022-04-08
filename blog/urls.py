ea680dabc7abe87ac7b069e6a8eed1ae209b9e00
woonsik <dnstlr2933@naver.com>
2022-04-08 17:23:09 +0900

    카테고리 링크 활성화, 태그 추가

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('', views.index),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
-    path('', include('single_pages.urls'))
+    path('', include('single_pages.urls')),
+    path('category/<str:slug>/', views.show_category_posts)
 ]
